9cc1dae142cdb97726ff63030740f5ed2c0786f2
Juan Silva <juan.silva.r@uni.pe>
2025-06-22 17:19:17 -0500

    chore: adding favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      ]
+    }
+  },
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
     css: ['~/assets/css/main.css'],
